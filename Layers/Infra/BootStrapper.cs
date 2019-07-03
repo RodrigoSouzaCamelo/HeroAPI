a3234a1bfeb395ccc1e16b4f040fd510b943ca06
Rodrigo Souza <rodrigo.souza.camelo@gmail.com>
2019-07-03 15:19:01 -0300

    Addition of the generic layer
    Adding the layer that will be inherited by all the contexts that will be added in the project

--- Layers/Infra/BootStrapper.cs
+++ Layers/Infra/BootStrapper.cs
@@ -1,8 +1,3 @@
-using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-
 namespace Layers.Infra.Infra
 {
     public static class BootStrapper
---
+++ UI/HeroAPI.WebAPI/Controllers/HeroController.cs
@@ -0,0 +1,17 @@
+﻿using HeroAPI.Application.Interfaces;
+using HeroAPI.Application.ViewModels;
+using HeroAPI.Domain.Entities;
+using Layers.WebApi.Controller;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : CrudController<Hero, HeroViewModel>
+    {
+        public HeroController(IHeroAppService appService) : base(appService)
+        {
+        }
+    }
+}
